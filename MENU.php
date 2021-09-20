1a5759aee447aaa43f6f9c642dcee0571c1fdc5a
Ingrid1109cruz <90727768+Ingrid1109cruz@users.noreply.github.com>
2021-09-20 12:10:08 -0500

    Add files via upload

---
+++ MENU.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <link rel="stylesheet" type="text/css" href="css\bootstrap.min.css">
+    <title>HUTZILOPOCHTLI SOFT </title>
+  </head>
+  <body>
+    <!--inicia el navbar de la pagina web-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">HUTZILOPOCHTLI SOFT </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">INICIO</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">¿QUIENES SOMOS?</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">SERVICIO A OFRECER </a>
+        </li>
+        <li class="nav-item dropdown">
+               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              VIDEOS DE GUIA
+               </a>
+               <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                 <li><a class="dropdown-item" href="#">Action</a></li>
+                 <li><a class="dropdown-item" href="#">Another action</a></li>
+                 <li><a class="dropdown-item" href="#">Something else here</a></li>
+                 </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">REDES SOCIALES </a>
+        </li>
+
+    </div>
+  </div>
+</nav>
+<!--termina mi navbar-->
+<script src="js\bootstrap.min.js"></script>
+    </body>
+</html>
